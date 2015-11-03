53d45aa0412673700f36a8d7e76f18fa2b85770d
dekaypvp <danikravchen@gmail.com>
2015-11-03 20:51:09 +0200

    Добавил новые классы для экрана меню, паузы и самой игры. И настроил взаимодействие между ними. Надо еще будет создать абстрактный класс AbsctractRenderer так как много общего с другими классами рендеринга. также вынес размер шариков в константы для удобности и количество шариков.

---
+++ core/src/com/dk/gametest1/GameScreen.java
@@ -0,0 +1,63 @@
+package com.dk.gametest1;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created by Крава on 03.11.2015.
+ */
+public class GameScreen extends AbstractScreen {
+    private GameRenderer gameRenderer;
+    private GameUpdater gameUpdater;
+    private boolean pause;
+
+    public GameScreen(Game game){
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        gameUpdater = new GameUpdater(game);
+        gameRenderer = new GameRenderer(gameUpdater);
+        Gdx.input.setCatchBackKey(true);
+        pause = false;
+    }
+
+    @Override
+    public void render(float delta) {
+        if(!pause){
+            gameUpdater.update(delta);
+        }
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        gameRenderer.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gameRenderer.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        pause = true;
+    }
+
+    @Override
+    public void hide() {
+        gameRenderer.dispose();
+        Gdx.input.setCatchBackKey(false);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+        pause = false;
+    }
+}
